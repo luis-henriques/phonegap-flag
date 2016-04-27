e991ee3f2ffb1aa9ad5a0eca5bb0bff86f0fcba3
Luis Henriques <luis.c.a.henriques@gmail.com>
2016-04-27 19:24:02 +0100

    added accelerometer

--- js/application.js
+++ js/application.js
@@ -1,23 +1,25 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var options = { frequency: 3000 };  // Update every 3 seconds
+
+var watchID = null;
+
 window.requestAnimationFrame(function () {
   new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
   document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     console.log(navigator.accelerometer);
+
+    function onSuccess(acceleration) {
+        console.log('Acceleration X: ' + acceleration.x + '\n' +
+              'Acceleration Y: ' + acceleration.y + '\n' +
+              'Acceleration Z: ' + acceleration.z + '\n' +
+              'Timestamp: '      + acceleration.timestamp + '\n');
+    };
+
+    function onError() {
+        alert('onError!');
+    };
+
+     watchID =navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 }
 });
-
-function onSuccess(acceleration) {
-    console.log('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
-};
-
-function onError() {
-    alert('onError!');
-};
-
-var options = { frequency: 3000 };  // Update every 3 seconds
-
-var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
