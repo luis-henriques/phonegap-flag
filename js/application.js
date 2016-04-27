6eecd23504bf4ed45113c8c68131a1cac0b5dae0
Luis Henriques <luis.c.a.henriques@gmail.com>
2016-04-27 19:27:45 +0100

    added accelerometer

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
-var options = { frequency: 3000 };  // Update every 3 seconds
+var options = { frequency: 1000 };  // Update every 3 seconds
 
 var watchID = null;
 
